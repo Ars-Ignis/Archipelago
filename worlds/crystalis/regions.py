a8518aad09a46ea297b933b5753106bc240f08ee
CodeGorilla <3672561+Ars-Ignis@users.noreply.github.com>
2024-06-01 22:07:06 -0500

    Implement some options, start logic
    getting distracted lmao
    
    - Shop shuffle
    - Proper regions and entrances for all Vm options and Rb
    - Thunder Warp
    - data tweaks

--- worlds/crystalis/regions.py
+++ worlds/crystalis/regions.py
@@ -11,7 +11,8 @@ def load_region_data_from_json() -> Dict[str, CrystalisRegionData]:
     return orjson.loads(pkgutil.get_data(__name__, "data/regions.json").decode("utf-8-sig"))
 
 
-regions_data = load_region_data_from_json()
+regions_data_json = load_region_data_from_json()
+regions_data: Dict[str, CrystalisRegionData] = {}
 #convert to actual type
-for key, value in regions_data.items():
-    regions_data[key] = CrystalisRegionData(**value)
+for key, value in regions_data_json.items():
+    regions_data[key] = CrystalisRegionData(value["name"], value["wildwarpIds"], value["entrances"], value["locations"])
