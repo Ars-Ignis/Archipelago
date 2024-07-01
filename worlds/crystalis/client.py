bd4feda395ed01a9d8bf6fcf9f954f74023c98db
CodeGorilla <3672561+Ars-Ignis@users.noreply.github.com>
2024-07-01 00:52:29 -0500

    client part 2 and other misc.
    -implemented receiving items from the server
    - get_filler_item_name == "Medical Herb"
    - tweak for No Bow Mode logic
    - add sword rule to every chest in oops all mimics
    - rename default option of Vanilla Wild Warp from "shuffled" to "disabled"

--- worlds/crystalis/client.py
+++ worlds/crystalis/client.py
@@ -1,8 +1,9 @@
-from typing import TYPE_CHECKING, List, Dict, Tuple, Set
-from NetUtils import ClientStatus
+from typing import TYPE_CHECKING, List, Dict, Tuple, Optional
+from NetUtils import ClientStatus, NetworkItem
 
 import worlds._bizhawk as bizhawk
 from worlds._bizhawk.client import BizHawkClient
+from .items import items_data_by_id
 from .regions import regions_data
 from .types import CRYSTALIS_BASE_ID
 
@@ -10,6 +11,11 @@ if TYPE_CHECKING:
     from worlds._bizhawk.context import BizHawkClientContext
 
 LOCATION_FLAGS_ADDR = 0x64A0
+RECEIVED_INDEX_ADDR = 0x657E
+SCREEN_MODE_ADDR = 0x0051
+END_OF_CONSUMABLE_INV_ADDR = 0x6447
+GET_ITEM_FLAG_ADDR = 0x6250
+GAME_MODE_ADDR = 0x40
 
 
 class CrystalisClient(BizHawkClient):
@@ -49,8 +55,10 @@ class CrystalisClient(BizHawkClient):
     async def game_watcher(self, ctx: "BizHawkClientContext") -> None:
         from CommonClient import logger
         try:
-            read_value = await bizhawk.guarded_read(ctx.bizhawk_ctx, [(LOCATION_FLAGS_ADDR, 16, "System Bus")],
-                                                        [(0x40, [1], "System Bus")])
+            read_value = await bizhawk.guarded_read(ctx.bizhawk_ctx, [(LOCATION_FLAGS_ADDR, 16, "System Bus"),
+                                                                      (RECEIVED_INDEX_ADDR, 2, "System Bus"),
+                                                                      (GET_ITEM_FLAG_ADDR, 1, "System Bus")],
+                                                        [(GAME_MODE_ADDR, [1], "System Bus")])
             if read_value is not None:
                 location_flags = read_value[0]
                 locations_to_send: List[int] = []
@@ -59,11 +67,38 @@ class CrystalisClient(BizHawkClient):
                     if location_flags[byte] & (1 << bit) != 0:
                         locations_to_send.append(location_id)
 
-                await ctx.send_msgs([{
-                        "cmd": "LocationChecks",
-                        "locations": list(locations_to_send)
-                    }])
+                if len(locations_to_send) > 0:
+                    await ctx.send_msgs([{
+                            "cmd": "LocationChecks",
+                            "locations": list(locations_to_send)
+                        }])
 
+                received_indices: bytes = read_value[1]
+                nonconsumable_index: int = received_indices[0]
+                consumable_index: int = received_indices[1]
+                get_item_flag: bool = read_value[2][0] != 0
+                if not get_item_flag:
+                    if nonconsumable_index + consumable_index < len(ctx.items_received):
+                        non_consumables = [item for item in ctx.items_received if
+                                           items_data_by_id[item.item].groups != ["Consumable"]]
+                        if nonconsumable_index < len(non_consumables):
+                            item_to_write: NetworkItem = non_consumables[nonconsumable_index]
+                            item_id: int = items_data_by_id[item_to_write.item].rom_id
+                            success: bool = await bizhawk.guarded_write(ctx.bizhawk_ctx,
+                                                        [(RECEIVED_INDEX_ADDR, [nonconsumable_index + 1], "System Bus"),
+                                                         (GET_ITEM_FLAG_ADDR, [1, item_id], "System Bus")],
+                                                        [(GAME_MODE_ADDR, [1], "System Bus")])
+                        else:
+                            consumables = [item for item in ctx.items_received if
+                                           items_data_by_id[item.item].groups == ["Consumable"]]
+                            if consumable_index < len(consumables):
+                                item_to_write: NetworkItem = consumables[consumable_index]
+                                item_id: int = items_data_by_id[item_to_write.item].rom_id
+                                success: bool = await bizhawk.guarded_write(ctx.bizhawk_ctx,
+                                                    [(RECEIVED_INDEX_ADDR + 1, [consumable_index + 1], "System Bus"),
+                                                     (GET_ITEM_FLAG_ADDR, [1, item_id], "System Bus")],
+                                                    [(GAME_MODE_ADDR, [1], "System Bus"),
+                                                     (END_OF_CONSUMABLE_INV_ADDR, [0xFF], "System Bus")])
         except bizhawk.RequestFailedError:
             # Exit handler and return to main loop to reconnect.
             pass
