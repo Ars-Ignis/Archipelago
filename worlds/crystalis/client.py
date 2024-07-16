a4eb416703432ebb97576df1ae098ece9ee8a0d6
CodeGorilla <3672561+Ars-Ignis@users.noreply.github.com>
2024-07-15 23:51:29 -0500

    Overwrite item flags for non-unique items to allow for receiving duplicates

--- worlds/crystalis/client.py
+++ worlds/crystalis/client.py
@@ -11,6 +11,7 @@ if TYPE_CHECKING:
     from worlds._bizhawk.context import BizHawkClientContext
 
 LOCATION_FLAGS_ADDR = 0x64A0
+ITEM_FLAGS_ADDR = 0x64C0
 RECEIVED_INDEX_ADDR = 0x657E
 SCREEN_MODE_ADDR = 0x0051
 END_OF_CONSUMABLE_INV_ADDR = 0x6447
@@ -24,18 +25,17 @@ class CrystalisClient(BizHawkClient):
     #intentionally not defining patch_suffix because Archipelago will not be responsible for patching the game
     expected_start: List[bytes] = [bytes([0xd9, 0xd9, 0xd9, 0xd9, 0xd9, 0xd9, 0xd9, 0xd9])]
     #TODO: Consider a better method for identifying valid Crystalis ROMs
-    id_to_addr: Dict[int, Tuple[int, int]] = {}
+    loc_id_to_addr: Dict[int, Tuple[int, int]] = {}
     unidentified_item_rom_ids: Dict[int, int] = {}
 
 
     def __init__(self):
         super().__init__()
-        #self.locally_checked_ids = set()
         for region in regions_data.values():
             for location in region.locations:
                 byte: int = location.rom_id // 8
                 bit: int = location.rom_id % 8
-                self.id_to_addr[location.ap_id_offset + CRYSTALIS_BASE_ID] = (byte, bit)
+                self.loc_id_to_addr[location.ap_id_offset + CRYSTALIS_BASE_ID] = (byte, bit)
 
 
     async def validate_rom(self, ctx: "BizHawkClientContext") -> bool:
@@ -69,17 +69,18 @@ class CrystalisClient(BizHawkClient):
 
         try:
             read_value = await bizhawk.read(ctx.bizhawk_ctx, [(LOCATION_FLAGS_ADDR, 16, "System Bus"),
+                                                              (ITEM_FLAGS_ADDR, 16, "System Bus"),
                                                               (RECEIVED_INDEX_ADDR, 2, "System Bus"),
                                                               (GET_ITEM_FLAG_ADDR, 1, "System Bus"),
                                                               (GAME_MODE_ADDR, 1, "System Bus")])
             if read_value is not None:
-                game_mode = read_value[3]
+                game_mode = read_value[4]
                 if game_mode[0] == 1: #GAME_MODE_NORMAL
 
                     location_flags = read_value[0]
                     locations_to_send: List[int] = []
                     for location_id in ctx.missing_locations:
-                        byte, bit = self.id_to_addr[location_id]
+                        byte, bit = self.loc_id_to_addr[location_id]
                         if location_flags[byte] & (1 << bit) != 0:
                             locations_to_send.append(location_id)
 
@@ -89,24 +90,32 @@ class CrystalisClient(BizHawkClient):
                                 "locations": list(locations_to_send)
                             }])
 
-                    received_indices: bytes = read_value[1]
+                    item_flags: bytes = read_value[1]
+                    received_indices: bytes = read_value[2]
                     nonconsumable_index: int = received_indices[0]
                     consumable_index: int = received_indices[1]
-                    get_item_flag: bool = read_value[2][0] != 0
+                    get_item_flag: bool = read_value[3][0] != 0
                     if not get_item_flag:
                         if nonconsumable_index + consumable_index < len(ctx.items_received):
                             non_consumables = [item for item in ctx.items_received if
                                                items_data_by_id[item.item].groups != ["Consumable"]]
                             if nonconsumable_index < len(non_consumables):
                                 item_to_write: NetworkItem = non_consumables[nonconsumable_index]
+                                unique: bool = items_data_by_id[item_to_write.item].unique
                                 item_id: int
                                 if item_to_write.item in self.unidentified_item_rom_ids.keys():
                                     item_id = self.unidentified_item_rom_ids[item_to_write.item]
                                 else:
                                     item_id = items_data_by_id[item_to_write.item].rom_id
+                                byte: int = item_id // 8
+                                bit: int = item_id % 8
+                                item_flag_byte: byte = item_flags[byte] if unique \
+                                    else item_flags[byte] & (0xFF ^ (1 << bit))
+
                                 await bizhawk.guarded_write(ctx.bizhawk_ctx,
                                                         [(RECEIVED_INDEX_ADDR, [nonconsumable_index + 1], "System Bus"),
-                                                         (GET_ITEM_FLAG_ADDR, [1, item_id], "System Bus")],
+                                                         (GET_ITEM_FLAG_ADDR, [1, item_id], "System Bus"),
+                                                         (ITEM_FLAGS_ADDR + byte, [item_flag_byte], "System Bus")],
                                                         [(GAME_MODE_ADDR, [1], "System Bus")])
                             else:
                                 consumables = [item for item in ctx.items_received if
@@ -114,9 +123,13 @@ class CrystalisClient(BizHawkClient):
                                 if consumable_index < len(consumables):
                                     item_to_write: NetworkItem = consumables[consumable_index]
                                     item_id: int = items_data_by_id[item_to_write.item].rom_id
-                                    bool = await bizhawk.guarded_write(ctx.bizhawk_ctx,
+                                    byte: int = item_id // 8
+                                    bit: int = item_id % 8
+                                    item_flag_byte: byte = item_flags[byte] & (0xFF ^ (1 << bit))
+                                    await bizhawk.guarded_write(ctx.bizhawk_ctx,
                                                        [(RECEIVED_INDEX_ADDR + 1, [consumable_index + 1], "System Bus"),
-                                                        (GET_ITEM_FLAG_ADDR, [1, item_id], "System Bus")],
+                                                        (GET_ITEM_FLAG_ADDR, [1, item_id], "System Bus"),
+                                                        (ITEM_FLAGS_ADDR + byte, [item_flag_byte], "System Bus")],
                                                        [(GAME_MODE_ADDR, [1], "System Bus"),
                                                         (END_OF_CONSUMABLE_INV_ADDR, [0xFF], "System Bus")])
                 elif game_mode == 0x1e and not ctx.finished_game: #GAME_MODE_DYNA_DEFEATED
