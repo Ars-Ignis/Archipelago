80f08b4041d36ce62df04f3433ea3bb58d8e17fe
CodeGorilla <3672561+Ars-Ignis@users.noreply.github.com>
2025-01-15 22:41:14 -0600

    Rework distribute_local_nonprogression
    `distribute_local_nonprogression` now calls `remaining_fill` on local non-progression items and non-priority locations, to properly respect item rules on locations. Additionally, instead of removing the locations from the list, locations filled this way now exist at the end of the location list.  `fill_restrictive` now interprets these locations correctly, and if it finds a location with an item already in it while trying to place progression, it finds a new suitable empty location to place the local filler in first, then places the progression item. This guarantees that sufficient room is always reserved for local filler while still allowing maximum room to breathe for progression, especially w.r.t. restrictive starts.

--- Main.py
+++ Main.py
@@ -205,8 +205,7 @@ def main(args, seed=None, baked_server_options: Optional[Dict[str, object]] = No
     if multiworld.algorithm == 'flood':
         flood_items(multiworld)  # different algo, biased towards early game progress items
     elif multiworld.algorithm == 'balanced':
-        distribute_items_restrictive(multiworld, get_settings().generator.panic_method,
-                                     get_settings().generator.sphere_1_percent_reserved_locations)
+        distribute_items_restrictive(multiworld, get_settings().generator.panic_method)
 
     AutoWorld.call_all(multiworld, 'post_fill')
 
